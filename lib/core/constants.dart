9499a3781e3c3003c80b51f759e4b9006fec3ff4
JosephMarzouk <joseph1marzok@gmail.com>
2025-04-29 18:39:41 +0300

    Finishing the Payment wallet and card logic

---
+++ lib/core/constants.dart
@@ -0,0 +1,14 @@
+
+class Constants {
+
+  String apiKey =
+    'ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SmpiR0Z6Y3lJNklrMWxjbU5vWVc1MElpd2ljSEp2Wm1sc1pWOXdheUk2TVRBek9EQTBNU3dpYm1GdFpTSTZJakUzTkRVek5ERTNPVFV1TWpjd09ETTNJbjAuRnVQZm9rMWhmbENFZTZKQl9KMzhhOXVyd2V2WlNCRzRERUdRV1NIdWJZb3MxSmV1d3FpQ0VnZk9MYUd6RVdiejNSV2tQT29hU3NPY2dJVWcwQktyVUE=';
+
+ String iframeId = '914372';
+
+ String integrationCardId = '5048139';
+
+ String integrationMobileWalletId = "5060586";
+
+}
+
